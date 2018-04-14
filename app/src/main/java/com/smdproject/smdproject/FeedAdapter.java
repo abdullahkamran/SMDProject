9a92529bff674bda212f4752663fc3cfabb092c5
abdullahkamran <kamranabdullah1998@gmail.com>
2018-04-14 20:37:06 +0500

    newsfeed in progress

---
+++ app/src/main/java/com/smdproject/smdproject/FeedAdapter.java
@@ -0,0 +1,55 @@
+package com.smdproject.smdproject;
+
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import java.util.List;
+
+import database.Post;
+
+/**
+ * Created by Abdullah on 2/28/2018.
+ */
+
+public class FeedAdapter extends RecyclerView.Adapter<FeedViewHolder> {
+
+    private List<Post> items;
+    private int itemLayout;
+
+    public FeedAdapter(List<Post> items, int itemLayout) {
+        this.items = items;
+        this.itemLayout = itemLayout;
+    }
+
+    @Override
+    public FeedViewHolder onCreateViewHolder(ViewGroup parent, int viewType){
+
+        View v= LayoutInflater.from(parent.getContext()).inflate(itemLayout,parent,false);
+        return new FeedViewHolder(v);
+    }
+
+    @Override
+    public void onBindViewHolder(FeedViewHolder holder,int position){
+
+        if(items!=null && holder!=null){
+            holder.dp.setImageURI(items.get(position).getPostman().dp);
+            holder.name.setText(items.get(position).getPostman().getName());
+            holder.nickname.setText("@"+items.get(position).getGroup().getNicknames().get(items.get(position).getPostman().getUid()));
+            holder.timestamp.setText(items.get(position).getStamp().getTimestamp().toString());
+            holder.text.setText(items.get(position).getText());
+
+        }
+
+    }
+
+    @Override
+    public int getItemCount(){
+
+        if(items!=null)
+            return items.size();
+        else return 0;
+    }
+
+}
---
+++ app/src/main/java/com/smdproject/smdproject/FeedViewHolder.java
@@ -0,0 +1,33 @@
+package com.smdproject.smdproject;
+
+import android.support.v7.widget.RecyclerView;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.ProgressBar;
+import android.widget.TextView;
+
+/**
+ * Created by Abdullah on 2/28/2018.
+ */
+
+public class FeedViewHolder extends RecyclerView.ViewHolder {
+
+    public ImageView dp;
+    public TextView name;
+    public TextView nickname;
+    public TextView timestamp;
+    public TextView text;
+
+
+    public FeedViewHolder(View view){
+        super(view);
+
+        dp=(ImageView)view.findViewById(R.id.dpOnPost);
+        name=(TextView)view.findViewById(R.id.nameOnPost);
+        nickname=(TextView)view.findViewById(R.id.nicknameOnPost);
+        timestamp=(TextView)view.findViewById(R.id.timestampOnPost);
+        text=(TextView)view.findViewById(R.id.textOnPost);
+
+    }
+
+}
