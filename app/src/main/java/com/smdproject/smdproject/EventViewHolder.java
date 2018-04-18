27d51594b4af8d38dfe4c0d7ad1b0c2901bef660
Ahmad Shahid <31070424+ahmad856@users.noreply.github.com>
2018-04-18 10:09:48 +0500

    updated

---
+++ app/src/main/java/com/smdproject/smdproject/EventViewHolder.java
@@ -0,0 +1,25 @@
+package com.smdproject.smdproject;
+
+import android.support.v7.widget.RecyclerView;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+public class EventViewHolder extends RecyclerView.ViewHolder {
+
+    public ImageView eventPic;
+    public TextView eventName;
+    public TextView timePlace;
+    public TextView intrPerson;
+    //public Button b;
+
+    public EventViewHolder(View view){
+        super(view);
+        eventPic = view.findViewById(R.id.eventImage);
+        eventName = view.findViewById(R.id.eventName);
+        timePlace = view.findViewById(R.id.eventTime);
+        intrPerson = view.findViewById(R.id.persons);
+
+    }
+}
