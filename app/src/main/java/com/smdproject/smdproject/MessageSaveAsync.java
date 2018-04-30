a10a7ee2d92886115cc02f6a0fe97cc3072e93bc
Ahmad Shahid <31070424+ahmad856@users.noreply.github.com>
2018-04-30 13:04:26 +0500

    updated bugs and fixes

--- app/src/main/java/com/smdproject/smdproject/MessageSaveAsync.java
+++ app/src/main/java/com/smdproject/smdproject/MessageSaveAsync.java
@@ -3,6 +3,8 @@ package com.smdproject.smdproject;
 import android.content.Context;
 import android.os.AsyncTask;
 
+import java.util.ArrayList;
+
 import database.Group;
 import database.Message;
 import database.MyDatabase;
@@ -16,9 +18,7 @@ public class MessageSaveAsync extends AsyncTask<Message,Void,Void> {
 
     @Override
     protected Void doInBackground(Message... messages) {
-
         MyDatabase myDb = MyDatabase.getAppDatabase(c);
-
         myDb.messageDao().insertAll(messages);
         return null;
     }
